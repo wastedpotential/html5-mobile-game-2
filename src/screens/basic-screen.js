73abfe06cc10b138823b6dac173ef5a894ba3a91
wastedpotential <andy@wastedpotential.com>
2022-10-02 21:18:57 -0400

    get simple fade animations working in navigator

--- src/screens/basic-screen.js
+++ src/screens/basic-screen.js
@@ -1,20 +1,37 @@
 // interface for screens
 import * as PIXI from '../scripts/pixi.js';
+import gsap from 'gsap';
 
 export class BasicScreen {
 	view = new PIXI.Container(); // wrapper view public for subclass access
+	filter = new PIXI.filters.AlphaFilter(0);
 
 	constructor() {
 		console.log('constructor');
+		this.view.filters = [this.filter];
 	}
 
 	show(onComplete) {
 		console.log('show', onComplete);
-		onComplete();
+		gsap.to(this.filter, {
+			alpha: 1,
+			delay: 0,
+			duration: 0.2,
+			onComplete: function () {
+				onComplete();
+			},
+		});
 	}
 
 	hide(onComplete) {
 		console.log('hide');
-		onComplete();
+		gsap.to(this.filter, {
+			alpha: 0,
+			delay: 0,
+			duration: 0.2,
+			onComplete: function () {
+				onComplete();
+			},
+		});
 	}
 }
